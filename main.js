c54eaaacb9f27fa081802a7aeec9a8624b799d66
Rahul Vishwakarma <rahulvishwakarma@Rahuls-MacBook-Pro.local>
2026-01-05 15:22:58 +0530

    added hourly data and managed it

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const APIs = {
   coordsAPI: (location) =>
     `https://nominatim.openstreetmap.org/search?q=${location}&format=json`,
   weatherAPI: (lat, lon) =>
-    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_min,temperature_2m_max&current=temperature_2m,weather_code,wind_speed_10m,relative_humidity_2m&timezone=auto`,
+    `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&daily=temperature_2m_min,temperature_2m_max&current=temperature_2m,weather_code,wind_speed_10m,relative_humidity_2m&timezone=auto&hourly=temperature_2m`,
 };
 
 const fetchUrl = async (url) => {
@@ -17,20 +17,47 @@ const getCoordinates = async (location) => {
   return { lat, lon };
 };
 
-const parseForecastData = (data) => {
-  return data.time.map((date, i) => ({
-    date,
-    tempMin: Math.round(data.temperature_2m_min[i]),
-    tempMax: Math.round(data.temperature_2m_max[i]),
-  }));
+const parseDailyForecast = (data, units) => {
+  const dailyData = [];
+  const { temperature_2m_min: minTempUnit, temperature_2m_max: maxTempUnit } =
+    units;
+  const { time, temperature_2m_min: minTemp, temperature_2m_max: maxTemp } =
+    data;
+  for (let i = 0; i < time.length; i++) {
+    dailyData.push({
+      date: time[i],
+      minTemp: `${Math.round(minTemp[i])}${minTempUnit}`.padStart(4, 0),
+      maxTemp: `${Math.round(maxTemp[i])}${maxTempUnit}`.padStart(4, 0),
+    });
+  }
+  return dailyData;
+};
+
+const parseHourlyTemp = (data, units) => {
+  const { time, temperature_2m } = data;
+  const { temperature_2m: tempUnit } = units;
+  const hours = time.slice(0, 24).map((el) => el.slice(-5));
+  const temp = temperature_2m.slice(0, 24).map((el) =>
+    `${Math.round(el)}${tempUnit} `.padStart(5, 0)
+  );
+
+  return { hours, temp };
 };
 
 const getWeatherData = async ({ lat, lon }) => {
   const url = APIs.weatherAPI(lat, lon);
-  const { current, current_units, daily, daily_units } = await fetchUrl(url);
+  const { current, current_units, daily, daily_units, hourly, hourly_units } =
+    await fetchUrl(url);
   const currentWeather = { units: current_units, data: current };
-  const forecast = { units: daily_units, data: parseForecastData(daily) };
-  return { currentWeather, forecast };
+  const hourlyForecast = {
+    units: hourly_units,
+    data: parseHourlyTemp(hourly, hourly_units),
+  };
+  const dailyForecast = {
+    units: daily_units,
+    data: parseDailyForecast(daily, daily_units),
+  };
+  return { currentWeather, hourlyForecast, dailyForecast };
 };
 
 const composeCurrentWeatherMsg = (currentWeather, location, { lat, lon }) => {
@@ -48,27 +75,39 @@ const composeCurrentWeatherMsg = (currentWeather, location, { lat, lon }) => {
   return msg;
 };
 
-const composeForecastMsg = (forecast) => {
-  const { temperature_2m_min: tempMinUnit, temperature_2m_max: tempMaxUnit } =
-    forecast.units;
-  const msgData = forecast.data.map(({ date, tempMin, tempMax }) => [
+const composeDailyForecastMsg = (forecast) => {
+  const msgData = forecast.data.map(({ date, minTemp, maxTemp }) => [
     date,
-    `${tempMin}${tempMinUnit}`,
-    `${tempMax}${tempMaxUnit}`,
+    minTemp,
+    maxTemp,
   ]);
   const msg = msgData.map((el) => el.join("   ")).join("\n");
   return msg;
 };
 
+const composeHourlyForecastMsg = (forecast) => {
+  const { data } = forecast;
+  const msg = Object.values(data).map((el) => el.join(" ")).join("\n");
+  return msg;
+};
+
 const displayCurrentWeather = (current, location, { lat, lon }) => {
   const msg = composeCurrentWeatherMsg(current, location, { lat, lon });
+  console.log("\n--- Current Weather ---\n");
   console.log(msg);
 };
 
-const displayForecast = (forecast) => {
-  const forecastMsg = composeForecastMsg(forecast);
+const displayDailyForecast = (forecast) => {
+  const forecastMsg = composeDailyForecastMsg(forecast);
   const title = `   Date      Min    Max   `;
+  console.log("\n--- Daily Temperature(min, max) ---\n");
   console.log(title);
+  console.log(forecastMsg);
+};
+
+const displayHourlyForecast = (forecast) => {
+  const forecastMsg = composeHourlyForecastMsg(forecast);
+  console.log("\n--- Hourly Temperature ---\n");
   console.log(forecastMsg);
 };
 
@@ -82,10 +121,12 @@ const main = async () => {
     main();
     return;
   }
-  const { currentWeather, forecast } = await getWeatherData(coordinates);
+  const { currentWeather, hourlyForecast, dailyForecast } =
+    await getWeatherData(coordinates);
 
   displayCurrentWeather(currentWeather, location, coordinates);
-  displayForecast(forecast);
+  displayHourlyForecast(hourlyForecast);
+  displayDailyForecast(dailyForecast);
 };
 
 main();
